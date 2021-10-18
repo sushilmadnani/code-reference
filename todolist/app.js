0dcf1c7e5afb218666b68ba7e3314af00028bfc4
Sushil Madnani <sushilmadnani@gmail.com>
2021-10-18 23:01:09 +0530

    add custom style for todolist application

--- todolist/app.js
+++ todolist/app.js
@@ -8,6 +8,7 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
 
