f021dcd1b27e35c3ffff6afbd034a03b53e85edf
Sushil Madnani <sushilmadnani@gmail.com>
2021-10-18 22:05:28 +0530

    replace var with let for correct scope

--- todolist/app.js
+++ todolist/app.js
@@ -9,22 +9,22 @@ app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.get("/", function(req, res){
-  var today = new Date();
+  let today = new Date();
    
-  var options = {
+  let options = {
 	  weekday: "long",
 	  day: "numeric",
 	  month: "long"
   };
-  var day = today.toLocaleDateString("en-US", options);
+  let day = today.toLocaleDateString("en-US", options);
   res.render("list", {day: day, items: items});
 });
 
 app.post("/", function(req, res){
-  var item = req.body.newItem;
+  let item = req.body.newItem;
   items.push(item);
   res.redirect("/");
 });
