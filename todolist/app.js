8fe960e3fba6db55db2bfcf553b73d776e6af9e2
Sushil Madnani <sushilmadnani@gmail.com>
2021-10-18 21:49:34 +0530

    add new todolist project using express and ejs

---
+++ todolist/app.js
@@ -0,0 +1,34 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
+app.get("/", function(req, res){
+  var today = new Date();
+   
+  var options = {
+	  weekday: "long",
+	  day: "numeric",
+	  month: "long"
+  };
+  var day = today.toLocaleDateString("en-US", options);
+  res.render("list", {day: day, items: items});
+});
+
+app.post("/", function(req, res){
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+});
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000.");
+});
