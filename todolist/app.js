284cd21511d1e270fc2a1f93349ae1f8d1fe4715
Sushil Madnani <sushilmadnani@gmail.com>
2021-10-19 14:00:41 +0530

    refactor code to include exports function

--- todolist/app.js
+++ todolist/app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
@@ -10,22 +11,15 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.get("/", function(req, res){
-  let today = new Date();
-   
-  let options = {
-	  weekday: "long",
-	  day: "numeric",
-	  month: "long"
-  };
-  let day = today.toLocaleDateString("en-US", options);
+  const day = date.getDate();
   res.render("list", {day: day, items: items});
 });
 
 app.post("/", function(req, res){
-  let item = req.body.newItem;
+  const item = req.body.newItem;
   items.push(item);
   res.redirect("/");
 });
