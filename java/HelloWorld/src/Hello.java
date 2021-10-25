9fa0cfe3d06bbfbd5e500ca8e316823399e6e17f
Sushil Madnani <sushilmadnani@gmail.com>
2021-10-25 09:47:07 +0530

    add an integer for test

--- java/HelloWorld/src/Hello.java
+++ java/HelloWorld/src/Hello.java
@@ -4,5 +4,8 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Sushil!");
+        
+        int myFirstNumber = 5;
+        System.out.println("myFirstNumber:" + myFirstNumber);
     }
 }
