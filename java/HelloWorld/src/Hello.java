31bbd4dc60f433528532f427a40cb7a6e0fded3a
Sushil Madnani <sushilmadnani@gmail.com>
2021-10-25 10:32:16 +0530

    Add few more code

--- java/HelloWorld/src/Hello.java
+++ java/HelloWorld/src/Hello.java
@@ -5,7 +5,16 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Sushil!");
         
-        int myFirstNumber = 5;
-        System.out.println("myFirstNumber:" + myFirstNumber);
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println("Total:" + myTotal);
+        
+        int myLastOne = 1000 - myTotal;
+        System.out.println("My Last One: "+myLastOne);
+        
+        
+
     }
 }
