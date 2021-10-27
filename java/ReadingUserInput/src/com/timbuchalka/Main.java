806f25db49134e9284d5bb3b54c787037121aeee
Sushil Madnani <sushilmadnani@gmail.com>
2021-10-27 22:08:29 +0530

    replace current year method used by java 8

--- java/ReadingUserInput/src/com/timbuchalka/Main.java
+++ java/ReadingUserInput/src/com/timbuchalka/Main.java
@@ -1,6 +1,6 @@
 package com.timbuchalka;
 
-import java.util.Calendar;
+import java.time.LocalDate;
 import java.util.Scanner;
 
 public class Main {
@@ -18,7 +18,7 @@ public class Main {
 
             System.out.println("Enter your name: ");
             String name = scanner.nextLine();
-            int age = (Calendar.getInstance().get(Calendar.YEAR)) - yearOfBirth;
+            int age = (LocalDate.now().getYear()) - yearOfBirth;
 
             if(age >= 0 && age <= 100) {
                 System.out.println("Your name is " + name + ", and you are " + age + " years old.");
