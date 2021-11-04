368753822a3c031c9f542717937934d2e1acb407
Sushil Madnani <sushilmadnani@gmail.com>
2021-11-04 09:52:06 +0530

    Java Collection Binary Search Example

--- java/Collections/src/com/timbuchalka/Theatre.java
+++ java/Collections/src/com/timbuchalka/Theatre.java
@@ -26,20 +26,25 @@ public class Theatre {
     }
 
     public boolean reserveSeat(String seatNumber) {
-        Seat requestedSeat = null;
-        for(Seat seat : seats) {
-            if(seat.getSeatNumber().equals(seatNumber)) {
-                requestedSeat = seat;
-                break;
-            }
-        }
-
-        if(requestedSeat == null) {
-            System.out.println("There is no seat " + seatNumber);
-            return false;
-        }
-
-        return requestedSeat.reserve();
+		/*
+		 * int low = 0; int high = seats.size()-1;
+		 * 
+		 * while (low <= high) { System.out.print("."); int mid = (low + high) / 2; Seat
+		 * midVal = seats.get(mid); int cmp =
+		 * midVal.getSeatNumber().compareTo(seatNumber);
+		 * 
+		 * if (cmp < 0) { low = mid + 1; } else if (cmp > 0) { high = mid - 1; } else {
+		 * return seats.get(mid).reserve(); } } System.out.println("There is no seat " +
+		 * seatNumber); return false;
+		 */
+    	Seat requestedSeat = new Seat(seatNumber);
+    	int foundSeat = Collections.binarySearch(seats, requestedSeat, null);
+    	if(foundSeat >= 0) {
+    		return seats.get(foundSeat).reserve();
+    	} else {
+    		System.out.println("There is no seat " + seatNumber);
+    		return false;
+    	}
     }
 
     // for testing
@@ -49,12 +54,17 @@ public class Theatre {
         }
     }
 
-    private class Seat {
+    private class Seat implements Comparable<Seat> {
         private final String seatNumber;
         private boolean reserved = false;
 
         public Seat(String seatNumber) {
             this.seatNumber = seatNumber;
+        }
+
+        @Override
+        public int compareTo(Seat seat) {
+            return this.seatNumber.compareToIgnoreCase(seat.getSeatNumber());
         }
 
         public boolean reserve() {
